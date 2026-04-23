16ecb363d83de0161ba04a05268c680b0e975c14
Khushbu <khushbupatan5@gmail.com>
2026-04-23 19:31:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Hey You Know What! You mean everything to me 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
